bac047da6dba7fb3a0ab8832cda9a3fe538cbf6f
Prince Senapati <princesenapati59@gmail.com>
2026-09-05 01:28:53 +0530

    added exception file and done more work in logger

---
+++ main.py
@@ -0,0 +1,14 @@
+from sensor.exception import SensorException
+import sys
+
+def test():
+    try:
+        a=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
+
+if __name__ == "__main__":
+    try:
+        test()
+    except Exception as e:
+        print(e)
---
+++ sensor/logger.py
@@ -0,0 +1,10 @@
+import logging 
+import os
+import sys
+from datetime import datetime
+
+logging.basicConfig(
+    filename=,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO()
+)
